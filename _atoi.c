a80f4ea2a4866f51d3b715d279d53fcc508b8775
evbotaokiemute@gmail.com <evbotaokiemute@gmail.com>
2022-12-25 12:18:51 -0800

    _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,5 +1,7 @@
 #include "shell.h"
 
+
+
 /**
  * interactive - returns true if shell is interactive mode
  * @info: struct address
@@ -7,8 +9,11 @@
  * Return: 1 if interactive mode, 0 otherwise
  */
 int interactive(info_t *info)
+
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+
+		return (isatty(STDIN_FILENO) && info->readfd <= 2);
+
 }
 
 /**
@@ -72,4 +77,3 @@ int _atoi(char *s)
 
 	return (output);
 }
-
--- errors1.c
+++ errors1.c
@@ -4,25 +4,24 @@
  * _erratoi - converts a string to an integer
  * @s: the string to be converted
  * Return: 0 if no numbers in string, converted number otherwise
- *       -1 on error
+ * -1 on error
  */
 int _erratoi(char *s)
 {
 	int i = 0;
 	unsigned long int result = 0;
 
-	if (*s == '+')
-		s++;  /* TODO: why does this make main return 255? */
+	if (*s == '+') s++; 
+	/* TODO: why does this make main return 255? */
 	for (i = 0;  s[i] != '\0'; i++)
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result += (s[i] - '0');
-			if (result > INT_MAX)
-				return (-1);
-		}
-		else
+
+		result *= 10;
+
+		result += (s[i] - '0');
+
+		if (result > INT_MAX)
+
 			return (-1);
 	}
 	return (result);
@@ -33,17 +32,18 @@ int _erratoi(char *s)
  * @info: the parameter & return info struct
  * @estr: string containing specified error type
  * Return: 0 if no numbers in string, converted number otherwise
- *        -1 on error
+ * -1 on error
  */
 void print_error(info_t *info, char *estr)
 {
 	_eputs(info->fname);
 	_eputs(": ");
-	print_d(info->line_count, STDERR_FILENO);
-	_eputs(": ");
-	_eputs(info->argv[0]);
-	_eputs(": ");
-	_eputs(estr);
+		print_d(info->line_count,
+				STDERR_FILENO);
+		_eputs(": ");
+		_eputs(info->argv[0]);
+		_eputs(": ");
+		_eputs(estr);
 }
 
 /**
@@ -51,7 +51,7 @@ void print_error(info_t *info, char *estr)
  * @input: the input
  * @fd: the filedescriptor to write to
  *
- * Return: number of characters printed
+ * Return: number of characters printed
  */
 int print_d(int input, int fd)
 {
@@ -69,21 +69,18 @@ int print_d(int input, int fd)
 	}
 	else
 		_abs_ = input;
+
 	current = _abs_;
 	for (i = 1000000000; i > 1; i /= 10)
 	{
 		if (_abs_ / i)
 		{
-			__putchar('0' + current / i);
-			count++;
-		}
-		current %= i;
-	}
-	__putchar('0' + current);
-	count++;
 
-	return (count);
-}
+			__putchar('0' + current);
+			count++;
+
+			return (count);
+		}
 
 /**
  * convert_number - converter function, a clone of itoa
@@ -96,7 +93,8 @@ int print_d(int input, int fd)
 char *convert_number(long int num, int base, int flags)
 {
 	static char *array;
-	static char buffer[50];
+	static char
+		buffer[50];
 	char sign = 0;
 	char *ptr;
 	unsigned long n = num;
@@ -107,14 +105,18 @@ char *convert_number(long int num, int base, int flags)
 		sign = '-';
 
 	}
-	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	array = flags & CONVERT_LOWERCASE ?
+		"0123456789abcdef" :
+		"0123456789ABCDEF";
 	ptr = &buffer[49];
 	*ptr = '\0';
 
-	do	{
+	do
+	{
 		*--ptr = array[n % base];
 		n /= base;
-	} while (n != 0);
+	}
+	while (n != 0);
 
 	if (sign)
 		*--ptr = sign;
@@ -134,8 +136,9 @@ void remove_comments(char *buf)
 	for (i = 0; buf[i] != '\0'; i++)
 		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
 		{
+
 			buf[i] = '\0';
+
 			break;
 		}
 }
-
