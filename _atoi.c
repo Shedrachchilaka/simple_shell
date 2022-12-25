14025c99d4155376d4356e20986463af6598a0e0
evbotaokiemute@gmail.com <evbotaokiemute@gmail.com>
2022-12-25 14:25:59 -0800

    _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -6,7 +6,6 @@
  *
  * Return: 1 if interactive mode, 0 otherwise
  */
-
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
@@ -15,30 +14,14 @@ int interactive(info_t *info)
 /**
  * is_delim - checks if character is a delimeter
  * @c: the char to check
- * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-
 int is_delim(char c, char *delim)
 {
 	while (*delim)
 		if (*delim++ == c)
 			return (1);
 	return (0);
-}
-
-/**
- * _isalpha - checks for alphabetic character
- * @c: The character to input
- * Return: 1 if c is alphabetic, 0 otherwise
- */
-
-int _isalpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
 }
 
 /**
@@ -88,3 +71,4 @@ int _atoi(char *s)
 
 	return (output);
 }
+
